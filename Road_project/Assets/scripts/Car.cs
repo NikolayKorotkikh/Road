2cb4141164dfd27ac2dec5cdecf465d8f58a7cba
Diana Ganieva <gaieva-diana@mail.ru>
2017-10-15 21:35:10 +0500

    создан тестовый кубик, прописан скрипт движения для машины

---
+++ Road_project/Assets/scripts/Car.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+
+    public float speed;
+    private Vector3 movement; //движение
+    public Rigidbody rb;
+    public Vector3 napravlenie;
+
+    void Start()
+    {
+    speed = 1.0F;
+    napravlenie = new Vector3(1, 0, 0);
+    rb = GetComponent<Rigidbody>();
+    }
+	
+	void Update ()//перемещение
+    {
+        movement = new Vector3(speed*napravlenie.x,0,0);//будем двигаться только по оси х
+	}
+
+    void FixedUpdate()
+    {
+        //применить движение к rigitbody
+        rb.velocity = movement;
+    }
+}
