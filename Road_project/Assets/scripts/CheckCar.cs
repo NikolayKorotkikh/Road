7841d8d43d96f3fe9c4e812ebf4685b7a0d2505b
Diana Ganieva <gaieva-diana@mail.ru>
2017-10-21 19:22:08 +0500

    Добавлен светофор, коллайдеры к нему, скрипт светофора, ведется работа над скриптами светофора и отлова количества машин и людей

---
+++ Road_project/Assets/scripts/CheckCar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCar : MonoBehaviour {
+
+    [SerializeField]
+    public byte CountCar;
+	// Update is called once per frame
+    
+    void OnTriggerEnter(Collider collider)
+    {
+        Car car = collider.GetComponent<Car>();
+        if (car)
+        {
+            CountCar++;
+            Debug.Log("машина попала в зону подсчета, сейчас их " + CountCar);
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        Car car = collider.GetComponent<Car>();
+        if (car)
+        {
+            CountCar--;
+            Debug.Log("машина вышла из зоны подсчета, сейчас их " + CountCar);
+        }
+    }
+}
---
+++ Road_project/Assets/scripts/Svetofor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Svetofor : MonoBehaviour {
+
+
+    [SerializeField]
+    public byte signalCar;
+    [SerializeField]
+    public byte signalHum;
+    [SerializeField]
+    Collider StopLineCar;
+    [SerializeField]
+    Collider StopLineHum;
+
+    // Use this for initialization
+    void Start ()
+    {
+        signalCar = 1;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //установка значений для пешеходного светофора на основе машинного
+        //1-красный, 2-желтый, 3-зеленый
+        if (signalCar == 1) { signalHum = 3; }
+        else if (signalCar == 2 || signalCar == 3) { signalHum = 1; }
+        else { signalHum = 1; Debug.Log("Подан неверный сигнал для светофора!"); }
+	}
+}
