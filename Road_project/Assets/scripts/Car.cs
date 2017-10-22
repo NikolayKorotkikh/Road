97bebe9b716fdf354e638dc959d1fd47c3dc5c4e
Diana Ganieva <gaieva-diana@mail.ru>
2017-10-22 15:08:52 +0500

    исправлены баги, добавлен 1 полностью работающий светофор, машина останавливается, если сигнал = 1(красный) или 2(желтый), при изменении на 3(зеленый) продолжает жвижение с прежней скоростью. менять сигнал светофора можно через объект Svetofor1 на главной сцене

--- Road_project/Assets/scripts/Car.cs
+++ Road_project/Assets/scripts/Car.cs
@@ -9,22 +9,40 @@ public class Car : MonoBehaviour {
     private Vector3 movement; //движение
     public Rigidbody rb;
     public Vector3 napravlenie;
+    float tempspeed;
 
     void Start()
     {
-    speed = 1.0F;
+    speed = 8.0F;
+    tempspeed = speed;
     napravlenie = new Vector3(1, 0, 0);
     rb = GetComponent<Rigidbody>();
     }
-	
-	void Update ()//перемещение
+
+    void Update()//перемещение
     {
-        movement = new Vector3(speed*napravlenie.x,0,0);//будем двигаться только по оси х
-	}
+        movement = new Vector3(speed * napravlenie.x, 0, 0);//будем двигаться только по оси х
+    }
 
     void FixedUpdate()
     {
         //применить движение к rigitbody
         rb.velocity = movement;
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        StopLine stop = collider.GetComponent<StopLine>();
+        if (stop && stop._signalCar==1) //при красном или желтом сигнале скорость=0
+        {
+            speed = 0;
+            Debug.Log("остановился, потому что красный или желтый");
+        }
+
+        if (stop && stop._signalCar == 3)//при зеленом сигнале восстанавливается начальная скорость
+        {
+            speed = tempspeed;
+            Debug.Log("поехал, потому что сигнал изменился на зеленый");
+        }
     }
 }
---
+++ Road_project/Assets/scripts/StopLine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopLine : MonoBehaviour {
+
+    //ссылка на свой светофор
+    [SerializeField]
+    Svetofor sv;
+
+    public byte _signalCar;
+    public byte _signalHum;
+    
+    void Update()
+    {
+        _signalCar = sv.signalCar;
+        _signalHum = sv.signalHum;
+    }
+
+}
