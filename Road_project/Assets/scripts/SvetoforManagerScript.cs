a8bf36f78de2ba99005b7ea01445961cc153ce39
Diana Ganieva <gaieva-diana@mail.ru>
2017-10-22 16:52:30 +0500

    добавлен менеджер светофоров, в котором будет устанавливаться сигнал для машин, (сигнал для пешеходов меняется автоматически), выведены данные по общему количеству машин и людей по обе стороны перекрестка

--- Road_project/Assets/scripts/SvetoforManagerScript.cs
+++ Road_project/Assets/scripts/SvetoforManagerScript.cs
@@ -4,13 +4,56 @@ using UnityEngine;
 
 public class SvetoforManagerScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+
+    [SerializeField]
+    Svetofor svetofor1;
+    [SerializeField]
+    Svetofor svetofor2;
+    [SerializeField]
+    CheckCar checkcar1;
+    [SerializeField]
+    CheckCar checkcar2;
+    [SerializeField]
+    CheckHum checkhum1;
+    [SerializeField]
+    CheckHum checkhum2;
+
+    //общие данные сигналов светофоров для данного перекрестка
+    [SerializeField]
+    byte signalcar;
+    [SerializeField]
+    byte signalhum;
+
+    //обобщенные данные количества машин и людей со всех светофоров
+    [SerializeField]
+    int countcar;
+    [SerializeField]
+    int counhum;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        signalcar = 1;
+        signalhum = 3;
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	private void Update ()
+    {
+        //установка значений светофорам для машин 
+        svetofor1.signalCar = signalcar;
+        svetofor2.signalCar = signalcar;
+        //подсчет общего количества машин и людей на светофорах
+        countcar= checkcar1.CountCar + checkcar2.CountCar;
+        counhum = checkhum1.CountHum + checkhum2.CountHum;
+
+
+        //установка значений для пешеходного светофора на основе машинного
+        //1-красный, 2-желтый, 3-зеленый
+        if (signalcar == 1) { signalhum = 3; }
+        else if (signalcar == 2 || signalcar == 3) { signalhum = 1; }
+        else { signalhum = 1; Debug.Log("Подан неверный сигнал для светофора!"); }
+
+    }
 }
