a77465ea0fc0f668338c5c74f9e1ebaa1779408f
Nikolay Korotkikh <nickkorotkikh@gmail.com>
2017-10-23 18:23:16 +0500

    Add respawn and timer

---
+++ Road_project/Assets/scripts/Respawn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+/// <summary>
+/// 
+/// </summary>
+public class Respawn {
+
+    private Queue<Car> _queueCars;
+    private Timers timer;
+    private int _time;
+
+    public Respawn(int _time) {
+        _queueCars = new Queue<Car>();
+        timer = new Timers();
+        this._time = _time;
+    }
+
+    public int Time
+    {
+        get { return _time; }
+        set { Time = value; }
+    }
+
+    public int QueueCarsLength {
+        get { return _queueCars.Count; }
+    }
+
+    public void Push(Car car) {
+        _queueCars.Enqueue(car);
+    }
+
+    public void Push(List<Car> cars)
+    {
+        foreach(Car car in cars)
+            _queueCars.Enqueue(car);
+    }
+
+    public Car Pop() {
+        timer.StartTimer(_time);
+        while (!timer.Elapse) { }
+        return _queueCars.Dequeue();
+    }
+}
---
+++ Road_project/Assets/scripts/Timers.cs
@@ -0,0 +1,27 @@
+﻿using System.Timers;
+
+/// <summary>
+/// 
+/// </summary>
+public class Timers {
+    private bool _elapse;
+    private Timer _timer;
+
+    public void StartTimer (int interval) {
+        _elapse = false;
+        _timer = new Timer();
+        _timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+        _timer.Interval += interval;
+	}
+
+    public bool Elapse
+    {
+        get { return _elapse; }
+    }
+
+    private void OnTimedEvent(object source, ElapsedEventArgs e)
+    {
+        _elapse = true;
+        _timer.Stop();
+    }
+}
