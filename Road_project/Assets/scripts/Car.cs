e3e61ac0c281db5f17f76f778a36491adab2dea7
Diana Ganieva <gaieva-diana@mail.ru>
2017-11-01 13:36:07 +0500

    добавлена моделька машины, исправлена модель светофора, реализована генерация машин с рандомными статами и рандомным появлением (пока только с одной стороны)

--- Road_project/Assets/scripts/Car.cs
+++ Road_project/Assets/scripts/Car.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class Car : MonoBehaviour {
 
-
     public float speed;
     private Vector3 movement; //движение
     public Rigidbody rb;
     public Vector3 napravlenie;
-    float tempspeed;
+    public float tempspeed;
+
 
     void Start()
     {
-    speed = 8.0F;
-    tempspeed = speed;
-    napravlenie = new Vector3(1, 0, 0);
-    rb = GetComponent<Rigidbody>();
+        //speed = 8.0F;
+        //tempspeed = speed;
+        //napravlenie = new Vector3(1, 0, 0);
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()//перемещение
@@ -33,16 +33,28 @@ public class Car : MonoBehaviour {
     private void OnTriggerStay(Collider collider)
     {
         StopLine stop = collider.GetComponent<StopLine>();
-        if (stop && (stop._signalCar==Svetofor.signalcar.red||stop._signalCar==Svetofor.signalcar.yellow)) //при красном или желтом сигнале скорость=0
+        Car car = collider.GetComponent<Car>();
+        if (car||stop && (stop._signalCar==Svetofor.signalcar.red||stop._signalCar==Svetofor.signalcar.yellow)) //при красном или желтом сигнале скорость=0
         {
             speed = 0;
-           // Debug.Log("красный или желтый для " + collider.name);
+           //Debug.Log("красный или желтый для " + collider.name);
         }
 
         if (stop && stop._signalCar == Svetofor.signalcar.green)//при зеленом сигнале восстанавливается начальная скорость
         {
             speed = tempspeed;
-            //Debug.Log("зеленый для " + collider.name);
+           // Debug.Log("зеленый для " + collider.name);
         }
     }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    Debug.Log("Collided with " + collision.gameObject.name);
+    //    Car car = collision.collider.GetComponent<Car>();
+    //    if (car)
+    //    {
+    //        speed=0;
+    //    }
+    //}
+
 }
---
+++ Road_project/Assets/scripts/CarRespawn.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarRespawn : MonoBehaviour {
+
+
+    float time;//счетчик времени
+    float savetime;
+    System.Random rnd = new System.Random();
+    int RndTime;//время в промежутках между генерацией, будет высчитываьбтся рандомно
+    float RndSpeed;
+    [SerializeField]
+    Vector3 GenNapr;
+    [SerializeField]
+    Car Carclone;
+
+    void Start()
+    {
+        RndTime = 0;
+        savetime = 0;
+    }
+
+    void Update()
+    {   //обновление времени
+        time = Time.time;
+        //Debug.Log(time);
+        //если текущее время равно последнему сохраненному времени + генерированное случайное число, то создать экземпляр машинки с заданными параметрами
+        if (time >= RndTime + savetime)
+        {
+            CreateCar();
+            RndTime = rnd.Next(1, 5);
+            savetime = time;
+            Debug.Log("через "+RndTime+" секунд поедет следующая машинка");
+            Debug.Log("сохраненное время"+time);
+        }
+    }
+
+    void CreateCar()
+    {
+        Debug.Log("создаю машинку");
+        Instantiate(Carclone);
+        RndSpeed = rnd.Next(5, 15);
+        Debug.Log("скорость машинки: "+RndSpeed);
+        Carclone.speed = RndSpeed;
+        Carclone.tempspeed = Carclone.speed;
+        Carclone.napravlenie = GenNapr;
+    }
+
+}
+	
+
